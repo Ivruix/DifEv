214c873c1d7e3b291889e2c8ce40efc636126dbe
Kaverin Maxim <kav.max007@gmail.com>
2020-01-21 19:15:22 +0300

    Initial commit

---
+++ difev/core.py
@@ -0,0 +1,91 @@
+from random import randint, sample, random, uniform
+
+
+class Population:
+    vectors = []
+    fitness = []
+
+    def __init__(self, num_of_vectors, num_of_parameters, bounds, differential_weight, crossover_probability,
+                 fitness_function, ensure_bounds=True):
+
+        for _ in range(num_of_vectors):
+            self.vectors.append(Vector([uniform(bounds[i][0], bounds[i][1]) for i in range(num_of_parameters)]))
+
+        for vector in self.vectors:
+            self.fitness.append(fitness_function(vector.coordinates))
+
+        self.bounds = bounds
+        self.differential_weight = differential_weight
+        self.crossover_probability = crossover_probability
+        self.num_of_parameters = num_of_parameters
+        self.num_of_vectors = num_of_vectors
+        self.fitness_function = fitness_function
+        self.ensure_bounds = ensure_bounds
+
+    def train(self, max_iterations, target_fitness, representation_interval=25, print_best_vector=False):
+
+        for i in range(max_iterations):
+            for new_vector in enumerate(self.__next_generation()):
+                vector_fitness = self.fitness_function(new_vector[1].coordinates)
+                if vector_fitness >= target_fitness:
+                    return new_vector[1].coordinates
+                if vector_fitness > self.fitness[new_vector[0]]:
+                    self.fitness[new_vector[0]] = vector_fitness
+                    self.vectors[new_vector[0]] = new_vector[1]
+            if representation_interval != 0:
+                if i % representation_interval == 0:
+                    self.__default_representation_function(i, print_best_vector)
+
+        return self.vectors[self.fitness.index(max(self.fitness))].coordinates
+
+    def __next_generation(self):
+
+        for i in range(self.num_of_vectors):
+
+            indexes = sample(range(self.num_of_vectors), 3)
+            while not (i in indexes):
+                indexes = sample(range(self.num_of_vectors), 3)
+            new_vector = self.vectors[indexes[0]] + \
+                         (self.vectors[indexes[1]] - self.vectors[indexes[2]]) * self.differential_weight
+            r = randint(0, self.num_of_parameters)
+            new_vector = Vector(
+                [e[1] if (random() < self.crossover_probability or e[0] == r) else self.vectors[i].coordinates[e[0]]
+                 for e in enumerate(new_vector.coordinates)])
+            if self.ensure_bounds:
+                new_vector = Vector([self.__ensure_bounds(e) for e in enumerate(new_vector.coordinates)])
+            yield new_vector
+
+    def __ensure_bounds(self, e):
+        if e[1] < self.bounds[e[0]][0]:
+            return self.bounds[e[0]][0]
+        elif e[1] > self.bounds[e[0]][1]:
+            return self.bounds[e[0]][1]
+        else:
+            return e[1]
+
+    def __default_representation_function(self, iteration, print_best_vector):
+        max_fitness = max(self.fitness)
+        best_vector = self.vectors[self.fitness.index(max_fitness)].coordinates
+        print("Generation:", iteration)
+        print("Max fitness:", round(max_fitness, 5))
+        print("Mean fitness", round(sum(self.fitness) / self.num_of_vectors, 5))
+        if print_best_vector:
+            print("Best vector:", best_vector)
+        print()
+
+
+class Vector:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+    def __add__(self, v):
+        return Vector([v.coordinates[i] + self.coordinates[i] for i in range(len(self.coordinates))])
+
+    def __mul__(self, k):
+        return Vector([i * k for i in self.coordinates])
+
+    def __sub__(self, v):
+        return Vector([self.coordinates[i] - v.coordinates[i] for i in range(len(self.coordinates))])
+
+    def __repr__(self):
+        return 'Vector ' + str(tuple(self.coordinates))
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='difev',
+      version='1.0',
+      author='Maxim K.',
+      author_email='kav.max007@gmail.com',
+      url='https://github.com/Ivruix/DifEv',
+      packages=['difev'],
+      )
